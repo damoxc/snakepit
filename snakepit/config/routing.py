73e7bde42cffa3df8d6a96c370b52ac4d247a178
Damien Churchill <damoxc@gmail.com>
2009-11-02 17:57:39 +0000

    add more minimal routes to the controllers

--- snakepit/config/routing.py
+++ snakepit/config/routing.py
@@ -20,6 +20,7 @@ def make_map():
 
     # CUSTOM ROUTES HERE
     map.connect('/', controller='main')
+    map.connect('/{controller}', controller='index')
     map.connect('/{controller}/{action}')
     map.connect('/{controller}/{action}/{id}')
 
