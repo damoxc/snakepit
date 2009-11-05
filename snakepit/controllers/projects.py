cebb9cffbc24a4863848c08a316f2d5b1ac6311f
Damien Churchill <damoxc@gmail.com>
2009-11-05 14:52:19 +0000

    add an initial way to add projects

--- snakepit/controllers/projects.py
+++ snakepit/controllers/projects.py
@@ -4,6 +4,7 @@ from pylons import request, response, session, tmpl_context as c
 from pylons.controllers.util import abort, redirect_to
 
 from snakepit.lib.base import BaseController, render
+from snakepit.model import db, Project
 
 log = logging.getLogger(__name__)
 
@@ -13,4 +14,35 @@ class ProjectsController(BaseController):
         return render('/projects/index.mao')
     
     def add(self):
+        if request.POST:
+            c.name = request.POST.get('name')
+            c.description = request.POST.get('description')
+            c.identifier = request.POST.get('identifier')
+            c.homepage = request.POST.get('homepage')
+            c.is_public = request.POST.get('is_public') or False
+            
+            if not c.name:
+                c.error = 'You must enter a name'
+                return render('/projects/add.mao')
+
+            if not c.identifier:
+                c.error = 'You must enter an identifier'
+                return render('/projects/add.mao')
+            
+            project = Project()
+            project.name = c.name
+            project.description = c.description
+            project.homepage = c.homepage
+            project.is_public = c.is_public
+            project.identifier = c.identifier
+            db.add(project)
+            db.commit()
+            redirect_to(action='show', id=c.identifier)
+        
         return render('/projects/add.mao')
+    
+    def show(self):
+        project = request.urlvars.get('id')
+        c.project = db.query(Project).filter_by(identifier=project).first()
+        return None
+        
--- snakepit/templates/projects/add.mao
+++ snakepit/templates/projects/add.mao
@@ -1,25 +1,28 @@
 <%inherit file="/base.mao" />
 <h2>New project</h2>
+% if c.error:
+	<div class="error">${c.error}</div>
+% endif
 <form action="${h.url_for()}" method="POST">
-	<label>Name ${h.required()}</label>
-	${h.tags.text('name')}
+	<label for="name">Name ${h.required()}</label>
+	${h.tags.text('name', c.name)}
 	<span class="description">30 characters maximum.</span>
 
-	<label>Description</label>
-	${h.tags.textarea('description')}
+	<label for="description">Description</label>
+	${h.tags.textarea('description', c.description)}
 
-	<label>Identifier ${h.required()}</label>
-	${h.tags.text('identifier')}
+	<label for="identifier">Identifier ${h.required()}</label>
+	${h.tags.text('identifier', c.identifier)}
 	<span class="description">
 		Length between 1 and 20 characters. Only lower case letters (a-z), numbers and dashes are allowed.<br/>
 		Once saved, the identifier can not be changed.
 	</span>
 
-	<label>Homepage</label>
-	${h.tags.text('homepage')}
+	<label for="homepage">Homepage</label>
+	${h.tags.text('homepage', c.homepage)}
 
-	<label>Public</label>
-	${h.tags.checkbox('public', checked=c.public or True)}
+	<label for="is_public">Public</label>
+	${h.tags.checkbox('is_public', checked=c.is_public or True)}
 
 	<button>Create &raquo;</button>
 </form>
